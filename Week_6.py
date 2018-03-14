cc2c49c800cab6b75443b2c79f428f30b89a9b84
eimearbutler7 <35781826+eimearbutler7@users.noreply.github.com>
2018-03-14 20:15:00 +0000

    Update Week_6.py

--- Week_6.py
+++ Week_6.py
@@ -1,22 +1,22 @@
 ###Formatting of Iris information ###http://archive.ics.uci.edu/ml/datasets/Iris
-##Solution 1
+#############  Solution 1  #######################
 
-f = open("iris.csv")
+f = open("iris.csv") #opens csv file
 
 for line in f:
-  line = line.replace(',', ' ')
-  line = line.rstrip()
-  print(line[:11], line[12:16], line[16:].strip())
+  line = line.replace(',', ' ') #for each line in the file, replace commas with spaces
+  line = line.rstrip() #strips whitespace form ends
+  print(line[:11], line[12:16], line[16:].strip()) #aligns columns evenly 
 
   
-f.close()
+f.close() #closes csv file
 
-##Solution 2
+############  Solution 2  ########################
 
-print ("petal length ", "petal width ", "sepal length ", "sepal width")
+print ("petal length ", "petal width ", "sepal length ", "sepal width") #adds titles
 
-with open ("iris.csv") as f:
-  for line in f:
+with open ("iris.csv") as f: #opens csv file
+  for line in f: #for each line in the file. And below: align column and remove commas 
     print ("{:>7} {:>12} {:>13} {:>12}" .format(line.split(',')[0],line.split(',')[1],line.split(',')[2],line.split(',')[3]))
 
-f.close()
+f.close() #closes csv file
