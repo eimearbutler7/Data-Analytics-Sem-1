8535d50cb2125e068c6f3368bcc60877907dc571
eimearbutler7 <35781826+eimearbutler7@users.noreply.github.com>
2018-03-14 20:10:28 +0000

    Update Week_5.py

--- Week_5.py
+++ Week_5.py
@@ -7,25 +7,31 @@
 
 i = 20
 
-while i > 1:
-  i += 20
+while i > 1: #i greater then 1 gurantees while loop will be used
+  i += 20  #the final number must be divisible by 20 so increasing by 20 is relevant and faster than increasing by 1
   if i % 11 == 0 and i % 12 == 0 and i % 13 == 0 and i % 14 == 0 and i % 15 == 0 and i % 16 == 0 and i % 17 == 0 and i % 18 == 0 and i % 19 == 0: 
-    break
+    break #condition to stop is when the number is divisible by 11 - 19 evenly also. 
 print(i) 
 
 
 ######### Solution 2 ###############################
 
 x = 1
-for i in range (1,21):
-    if (x % i) > 0: 
-        for s in range (1,21):
-            if (x * s) % i == 0:    
-                x = x * s
-                break
-print (x)     
-    
-######## Solution 3 - in progress, to test #####################
+for i in range (1,21): #rane function means loop will repeat throuh i being 1-20
+    if (x % i) > 0: #where x / i results in an even division
+        for s in range (1,21): 
+            if (x * s) % i == 0: #where (x(1) * s(1-20) / i(1-20) results in an even division    
+                x = x * s #increase x by a multiplying it by the value of s which should speed up the loops as the result will be the next largest divisible number as the loop continues
+                break #end when lwest number divisible by all numbers found
+print (x)     #print answer
+
+
+
+
+
+
+
+######## Solution 3 - IN PROGRESS, to test #####################
 
 i = 20
 
@@ -79,7 +85,7 @@ for i in a[1:]:
   lcm = lcm*i/gcd(lcm, i)
 print lcm
 
-#Project Euler Question 1
+#Project Euler Question 1  #Ian McLoughlan Example
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000
 
